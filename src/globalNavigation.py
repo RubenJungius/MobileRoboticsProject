8a5594828d7c814d18756daa000e223db1ddd6b8
Ruben <ruben.jungius@epfl.ch>
2023-11-29 16:53:30 +0100

    cleanup v2

--- src/globalNavigation.py
+++ src/globalNavigation.py
@@ -30,8 +30,25 @@ def generateGraph(image, start, start_node, target, target_node, offset):
         init_graph[i[0]][i[1]]=i[2]
     graph= Graph.Graph(nodes, init_graph)
 
-    return polygons, large_polygons, graph, connections, nodes_list
+    return graph, connections, nodes_list
     
+def draw_graph(image, connections, nodelist, path):
+    plt.figure()
+    plt.imshow(image)
+    for i in connections:
+        plt.plot([nodelist[i[0]][0], nodelist[i[1]][0]], [nodelist[i[0]][1], nodelist[i[1]][1]], 'ro-')
+
+    # Plot shortest Path
+    for i in range(len(path)-1):
+        plt.plot([nodelist[path[i]][0], nodelist[path[i+1]][0]],[nodelist[path[i]][1], nodelist[path[i+1]][1]], 'bo-')
+    plt.show()
+
+def run_global(image, start_node, start, target_node, target, offset):
+    graph, connections, nodelist= generateGraph(image, start, start_node, target, target_node, offset)
+    previous_node, shortest_path = Dijkstra.findPath(graph, start_node)
+    path = Dijkstra.get_shortest(previous_node, start_node, target_node)
+    return path, connections, nodelist
+
 # TestCode
 start_node = 0
 target_node = 1
@@ -41,29 +58,5 @@ offset = 30
 start = [160,700]
 target = [950,180]
 
-polygons, large_polygons, graph, connections, nodelist= generateGraph(image, start, start_node, target, target_node, offset)
-previous_node, shortest_path = Dijkstra.findPath(graph, start_node)
-path = Dijkstra.get_shortest(previous_node, start_node, target_node)
-
-plt.figure()
-plt.imshow(image)
-
-# Draw Enlarged Polygons
-'''
-count = 2
-for i in large_polygons:
-    for j in range(len(large_polygons[i])-1):
-        plt.plot([large_polygons[i][count][0], large_polygons[i][count+1][0]], 
-                 [large_polygons[i][count][1], large_polygons[i][count+1][1]], 'bo-')
-        count += 1
-    plt.plot([large_polygons[i][count-len(large_polygons[i])+1][0], large_polygons[i][count][0]], 
-             [large_polygons[i][count-len(large_polygons[i])+1][1], large_polygons[i][count][1]], 'bo-')
-    count += 1
-'''
-for i in connections:
-    plt.plot([nodelist[i[0]][0], nodelist[i[1]][0]], [nodelist[i[0]][1], nodelist[i[1]][1]], 'ro-')
-
-# Plot shortest Path
-for i in range(len(path)-1):
-    plt.plot([nodelist[path[i]][0], nodelist[path[i+1]][0]],[nodelist[path[i]][1], nodelist[path[i+1]][1]], 'bo-')
-plt.show()
+path, connections, nodelist = run_global(image, start_node, start, target_node, target, offset)
+draw_graph(image, connections, nodelist, path)
